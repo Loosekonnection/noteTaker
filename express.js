d9b8238ee1ca274accec0f964fb93207bd2bc7d1
Loosekonnection <david.bates@loosekonnection.co.uk>
2021-03-06 14:36:07 +0000

    server side now working

--- express.js
+++ express.js
@@ -25,7 +25,44 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.htm
 // GET api/notes from db.json
 app.get('/api/notes', (req, res) => res.json(notesObj));
 
+// POST api/note and add a note id
+app.post('/api/notes', (req, res) => {
+    
+    let addNote = req.body;
+    let noteId = 0;
+    
+    for (let i = 0; i < notesObj.length; i++) {
 
+        let note = notesObj[i];
+
+        if (note.id > noteId) {
+            noteId = note.id;
+        }
+    };
+
+    addNote.id = noteId + 1;
+    notesObj.push(addNote);
+    fs.writeFile(dbJson, JSON.stringify(notesObj), error =>
+    error ? console.log(error) : console.log('Note saved.'));
+    res.json(addNote);
+});
+
+// DELETE api/note using matching id
+app.delete('/api/notes/:id', (req, res) => {
+
+    const lastId = req.params.id;
+    
+    for (let i = 0; i < notesObj.length; i++) {
+        if (notesObj[i].id == lastId) { 
+            notesObj.splice(i, 1);
+            break;
+        }
+    };
+    
+    fs.writeFile(dbJson, JSON.stringify(notesObj), error =>
+    error ? console.log(error) : console.log('Note deleted!'));
+    res.json(notesObj);
+});
 
 // Starts the server listening on configured port
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
