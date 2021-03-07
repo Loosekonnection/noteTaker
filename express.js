6508548bf6c7d5edd5dfad17fa8ae09acdef7fb0
Loosekonnection <david.bates@loosekonnection.co.uk>
2021-03-07 11:30:39 +0000

    Comment Changes

--- express.js
+++ express.js
@@ -3,10 +3,10 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const notesObj = require('./db/db');
-
 const dbJson = path.join(__dirname, './db/db.json');
 
-// Sets up the Express App
+// Sets up the Express App - for local usage port 5500 is in use
+// Use https://localhost:5500 in a local web browser once running
 const app = express();
 const PORT = process.env.PORT || 5500;
 
@@ -26,10 +26,10 @@ app.get('/api/notes', (req, res) => res.json(notesObj));
 
 // POST api/note and add a note id to each note
 app.post('/api/notes', (req, res) => {
-    
+
     let addNote = req.body;
     let noteId = 0;
-    
+
     for (let i = 0; i < notesObj.length; i++) {
 
         let note = notesObj[i];
@@ -42,7 +42,7 @@ app.post('/api/notes', (req, res) => {
     addNote.id = noteId + 1;
     notesObj.push(addNote);
     fs.writeFile(dbJson, JSON.stringify(notesObj), error =>
-    error ? console.log(error) : console.log('Note saved.'));
+        error ? console.log(error) : console.log('Note saved.'));
     res.json(addNote);
 });
 
@@ -50,18 +50,18 @@ app.post('/api/notes', (req, res) => {
 app.delete('/api/notes/:id', (req, res) => {
 
     const lastId = req.params.id;
-    
+
     for (let i = 0; i < notesObj.length; i++) {
-        if (notesObj[i].id == lastId) { 
+        if (notesObj[i].id == lastId) {
             notesObj.splice(i, 1);
             break;
         }
     };
-    
+
     fs.writeFile(dbJson, JSON.stringify(notesObj), error =>
-    error ? console.log(error) : console.log('Note deleted!'));
+        error ? console.log(error) : console.log('Note deleted!'));
     res.json(notesObj);
 });
 
-// Starts the server listening on configured port
+// Starts the server listening on configured port and console logs the port
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
